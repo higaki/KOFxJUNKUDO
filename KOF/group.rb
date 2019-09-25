14ec256d6e764b36993a5fe99903ca529173c9fc
higaki <mas.higa@gmail.com>
2019-09-25 16:41:02 +0900

    Add KOF::Group#==

--- KOF/group.rb
+++ KOF/group.rb
@@ -4,14 +4,21 @@ module KOF
   class Group
     def initialize(name, email, id = nil, booth = nil, seminar = nil, junku = nil)
       @id, @name, @email, @booth, @seminar, @junku =
-        id, name, email, booth, seminar, junku
+        id&.to_i, name, email, booth, seminar, junku
     end
 
     def key
       [name, email]
     end
 
-    attr_accessor *%i[id name email booth seminar junku]
+    attr_accessor(*%i[id name email booth seminar junku])
+
+    def == o
+      factors = %i[id name email booth seminar junku]
+      factors.map{|i|self.__send__(i)} == factors.map{|i|o.__send__(i)}
+    rescue
+      false
+    end
 
     def self.read(fn)
       open(fn, &:readlines)
@@ -20,10 +27,52 @@ module KOF
             booth   = nil  if booth&.empty?
             seminar = nil  if seminar&.empty?
             junku   = nil  if junku&.empty?
-            KOF::Group.new(name, email, id.to_i, booth, seminar, junku)}
+            KOF::Group.new(name, email, id, booth, seminar, junku)
+        }
         .inject({}){|gs, g| gs[g.key] = g; gs}
     rescue SystemCallError
       {}
     end
   end
 end
+
+
+if $0 == __FILE__
+  require 'test/unit'
+
+  class TestGroup <Test::Unit::TestCase
+    def test_equal
+      [ ["a", "a@a", nil, nil, nil, nil,
+         "a", "a@a", nil, nil, nil, nil,  true],
+        ["a", "a@a", nil, nil, nil, nil,
+         "b", "a@a", nil, nil, nil, nil,  false],
+        ["a", "a@a", nil, nil, nil, nil,
+         "a", "b@a", nil, nil, nil, nil,  false],
+        ["a", "a@a", nil, nil, nil, nil,
+         "a", "a@a",   1, nil, nil, nil,  false],
+        ["a", "a@a", nil, nil, nil, nil,
+         "a", "a@a", nil,   2, nil, nil,  false],
+        ["a", "a@a", nil, nil, nil, nil,
+         "a", "a@a", nil, nil,   3, nil,  false],
+        ["a", "a@a", nil, nil, nil, nil,
+         "a", "a@a", nil, nil, nil,   4,  false],
+      ].each do |i|
+        lhs = KOF::Group.new(*[*0.. 5].map{|j|i[j]})
+        rhs = KOF::Group.new(*[*6..11].map{|j|i[j]})
+        assert_equal i.last, lhs == rhs, i
+      end
+
+      assert_false KOF::Group.new("a", "a@a") == ["a", "a@a"]
+    end
+  end
+end
+
+# >> Loaded suite /home/higaki/kof/KOFxJUNKUDO/KOF/xmpfilter.tmpfile_7588-1
+# >> Started
+# >> ..
+# >> Finished in 0.0007476 seconds.
+# >> -------------------------------------------------------------------------------
+# >> 2 tests, 13 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
+# >> 100% passed
+# >> -------------------------------------------------------------------------------
+# >> 2675.23 tests/s, 17388.98 assertions/s

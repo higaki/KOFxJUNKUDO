354a1a0c7c6738dbcbb205c5668f214362cb5e03
higaki <mas.higa@gmail.com>
2019-10-08 14:58:41 +0900

    Add publisher in KOF::Book

--- KOF/book.rb
+++ KOF/book.rb
@@ -2,21 +2,24 @@
 
 module KOF
   class Book
-    def initialize(isbn, title, url)
-      @isbn, @title, @url = isbn.gsub('-', '').upcase, title || '', url || ''
+    def initialize(isbn, title, url, publisher)
+      @isbn  = isbn.gsub('-', '').upcase
+      @title = title || ''
+      @url   = url || ''
+      @publisher = publisher || ''
     end
 
-    attr_accessor :isbn, :title, :url
+    attr_accessor :isbn, :title, :url, :publisher
 
     def == o
-      [isbn, title, url] == [o.isbn, o.title, o.url]
+      [isbn, title, url, publisher] == [o.isbn, o.title, o.url, o.publisher]
     rescue
       false
     end
 
     def self.read(io)
       io.map{|l| l.strip.split(/\t/)}
-        .map{|isbn, title, url| KOF::Book.new(isbn, title, url)}
+        .map{|isbn, title, url, pub| KOF::Book.new(isbn, title, url, pub)}
         .inject({}){|bs, b| bs[b.isbn] = b; bs}
     rescue SystemCallError
       {}
@@ -24,7 +27,7 @@ module KOF
 
     def self.add(isbn, books)
       isbn = isbn.gsub('-', '').upcase
-      books[isbn] = Book.new(isbn, '', '') unless books.include? isbn
+      books[isbn] = Book.new(isbn, '', '', '') unless books.include? isbn
       books[isbn]
     end
   end
@@ -37,43 +40,45 @@ if $0 == __FILE__
 
   class TestBook <Test::Unit::TestCase
     def test_new
-      [ ["401X", "Alfa",  "alfa",  KOF::Book.new("401X",   "Alfa",  "alfa")],
-        ["401X", "Alfa",  "alfa",  KOF::Book.new("4-01-X", "Alfa",  "alfa")],
-        ["401X", "Alfa",  "alfa",  KOF::Book.new("401x",   "Alfa",  "alfa")],
-        ["402X", "Bravo", "bravo", KOF::Book.new("402X",   "Bravo", "bravo")],
-      ].each do |isbn, title, url, actual|
+      [ ["401X", "Alfa",  "alfa",  "ALFA",  KOF::Book.new("401X",   "Alfa",  "alfa",  "ALFA")],
+        ["401X", "Alfa",  "alfa",  "ALFA",  KOF::Book.new("4-01-X", "Alfa",  "alfa",  "ALFA")],
+        ["401X", "Alfa",  "alfa",  "ALFA",  KOF::Book.new("401x",   "Alfa",  "alfa",  "ALFA")],
+        ["402X", "Bravo", "bravo", "BRAVO", KOF::Book.new("402X",   "Bravo", "bravo", "BRAVO")],
+      ].each do |isbn, title, url, pub, actual|
         assert_equal isbn,  actual.isbn
         assert_equal title, actual.title
         assert_equal url,   actual.url
+        assert_equal pub,   actual.publisher
       end
     end
 
     def test_equal
-      [ ["401X", "Alfa",  "alfa",   "401X", "Alfa",  "alfa",  true],
-        ["401X", "Alfa",  "alfa",   "402X", "Alfa",  "alfa",  false],
-        ["401X", "Alfa",  "alfa",   "401X", "Bravo", "alfa",  false],
-        ["401X", "Alfa",  "alfa",   "401X", "Alfa",  "bravo", false],
-        ["401X", "Alfa",  "alfa", "4-01-X", "Alfa",  "alfa",  true],
+      [ ["401X", "Alfa",  "alfa", "ALFA",   "401X", "Alfa",  "alfa",  "ALFA",  true],
+        ["401X", "Alfa",  "alfa", "ALFA",   "402X", "Alfa",  "alfa",  "ALFA",  false],
+        ["401X", "Alfa",  "alfa", "ALFA",   "401X", "Bravo", "alfa",  "ALFA",  false],
+        ["401X", "Alfa",  "alfa", "ALFA",   "401X", "Alfa",  "bravo", "ALFA",  false],
+        ["401X", "Alfa",  "alfa", "ALFA",   "401X", "Alfa",  "alfa",  "BRAVO", false],
+        ["401X", "Alfa",  "alfa", "ALFA", "4-01-X", "Alfa",  "alfa",  "ALFA",  true],
       ].each do |i|
-        lhs = KOF::Book.new(*i[0..2])
-        rhs = KOF::Book.new(*i[3..5])
+        lhs = KOF::Book.new(*i[0..3])
+        rhs = KOF::Book.new(*i[4..7])
         assert_equal i.last, lhs == rhs, i
       end
-      assert_false KOF::Book.new("401X", "Alfa", "http://honto.jp/alfa") == %w[401X Alfa http://honto.jp/alfa]
+      assert_false KOF::Book.new("401X", "Alfa", "http://honto.jp/alfa", "ALFA") == %w[401X Alfa http://honto.jp/alfa]
     end
 
     def test_read_1book
-      actual = KOF::Book.read(StringIO.new("401X\tAlfa\talfa"))
+      actual = KOF::Book.read(StringIO.new("401X\tAlfa\talfa\tALFA"))
 
-      expectation = {"401X" => KOF::Book.new("401X", "Alfa", "alfa")}
+      expectation = {"401X" => KOF::Book.new("401X", "Alfa", "alfa", "ALFA")}
       assert_equal expectation, actual
     end
 
     def test_read_2books
-      actual = KOF::Book.read(StringIO.new("401X\tAlfa\talfa\n402X\tBravo\tbravo"))
+      actual = KOF::Book.read(StringIO.new("401X\tAlfa\talfa\tALFA\n402X\tBravo\tbravo\tBRAVO"))
       expectation = {
-        "401X" => KOF::Book.new("401X", "Alfa",  "alfa"),
-        "402X" => KOF::Book.new("402X", "Bravo", "bravo"),
+        "401X" => KOF::Book.new("401X", "Alfa",  "alfa",  "ALFA"),
+        "402X" => KOF::Book.new("402X", "Bravo", "bravo", "BRAVO"),
       }
       assert_equal expectation, actual
     end
@@ -102,8 +107,8 @@ if $0 == __FILE__
 
     def test_add_already_exists
       books = {
-        "9784774193977" => KOF::Book.new("9784774193977", "プロを目指す人のための Ruby 入門", "https://honto.jp/netstore/pd-book_28745880.html"),
-        "9784862464149" => KOF::Book.new("9784862464149", "クリエイターのための権利の本", "https://honto.jp/netstore/pd-book_29253824.html"),
+        "9784774193977" => KOF::Book.new("9784774193977", "プロを目指す人のための Ruby 入門", "https://honto.jp/netstore/pd-book_28745880.html", "技術評論社"),
+        "9784862464149" => KOF::Book.new("9784862464149", "クリエイターのための権利の本",     "https://honto.jp/netstore/pd-book_29253824.html", " ボーンデジタル"),
       }
 
       assert_not_equal "", books["9784774193977"].title
@@ -127,12 +132,12 @@ if $0 == __FILE__
   end
 end
 
-# >> Loaded suite /home/higaki/kof/KOFxJUNKUDO/KOF/xmpfilter.tmpfile_2403-1
+# >> Loaded suite /home/higaki/kof/KOFxJUNKUDO/KOF/xmpfilter.tmpfile_12013-1
 # >> Started
 # >> .......
-# >> Finished in 0.0017355 seconds.
+# >> Finished in 0.0017095 seconds.
 # >> -------------------------------------------------------------------------------
-# >> 7 tests, 37 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
+# >> 7 tests, 42 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
 # >> 100% passed
 # >> -------------------------------------------------------------------------------
-# >> 4033.42 tests/s, 21319.50 assertions/s
+# >> 4094.76 tests/s, 24568.59 assertions/s

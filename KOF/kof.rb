c9f5cfd4ef96228742e818ee4504cd675e1d11e9
HIGAKI Masaru <mas.higa@gmail.com>
2019-10-19 17:17:13 +0900

    Fix to_times

--- KOF/kof.rb
+++ KOF/kof.rb
@@ -98,9 +98,44 @@ module KOF
         [Time.local(2019, 11, 8, 0, 0, 0), nil]
       when %r|.*11/9|
         [nil, Time.local(2019, 11, 9, 0, 0, 0)]
-      when /両日|どちらの日でもよい/
+      when /.*11月(\d+)日\s+(\d+)時/
+        t = Time.local(2019, 11, $1.to_i, $2.to_i, 0, 0)
+        t.mday == 8 ? [t, nil] : [nil, t]
+      when /両日|どちらの日でもよい/
         [8, 9].map{|i| Time.local(2019, 11, i, 0, 0, 0)}
       end
     end
   end
 end
+
+if $0 == __FILE__
+  require 'test/unit'
+  using KOF
+
+  class TestKOF <Test::Unit::TestCase
+    def test_to_times
+      fri = Time.local(2019, 11, 8,  0, 0, 0)
+      sat = Time.local(2019, 11, 9,  0, 0, 0)
+      [ ["11月8日 16時",      [Time.local(2019, 11, 8, 16, 0, 0), nil]],
+        ["11月9日 11時",      [nil, Time.local(2019, 11, 9, 11, 0, 0)]],
+        ["2019/11/8(金曜日)", [fri, nil]],
+        ["2019/11/9(土曜日)", [nil, sat]],
+        ["どちらの日でもよい",  [fri, sat]],
+        ["11/8 (金曜日) のみ", [fri, nil]],
+        ["11/9 (土曜日) のみ", [nil, sat]],
+        ["両日",              [fri, sat]],
+      ].each do |src, exp|
+        assert_equal exp, src.to_times
+      end
+    end
+  end
+end
+# >> Loaded suite -
+# >> Started
+# >> .
+# >> Finished in 0.000443 seconds.
+# >> -------------------------------------------------------------------------------
+# >> 1 tests, 8 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
+# >> 100% passed
+# >> -------------------------------------------------------------------------------
+# >> 2257.34 tests/s, 18058.69 assertions/s

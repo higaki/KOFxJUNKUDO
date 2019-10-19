e7c8b011a7f096cc95f307340d1e2a0bb04dd675
HIGAKI Masaru <mas.higa@gmail.com>
2019-10-19 22:33:11 +0900

    Add to_hyperlink in KOF::Seminar

--- KOF/seminar.rb
+++ KOF/seminar.rb
@@ -1,6 +1,5 @@
 #! /usr/bin/env ruby
 
-require 'date'
 require_relative './kof'
 
 module KOF
@@ -15,12 +14,19 @@ module KOF
       end
     end
 
-    def initialize(group, user, email, title, abstract, talker, preferred_date, scale, appendix,date, room)
-      @group, @user, @email, @title, @abstract, @talker, @preferred_date, @scale, @appendix, @date, @room =
-        group.regularize, user.regularize, email, title, abstract, talker, preferred_date.to_times, scale, appendix, (DateTime.parse(date).to_time rescue nil), room
+    def initialize(group, user, email, title, abstract, talker, preferred_date, scale, appendix, room, date)
+      @group, @user, @email, @title, @abstract, @talker, @preferred_date, @scale, @appendix, @room, @dates =
+        group.regularize, user.regularize, email, title, abstract, talker, preferred_date.to_times, scale, appendix, room, date.to_times
     end
 
-    attr_reader :group, :user, :email, :title, :abstract, :preferred_date, :date
+    def to_hyperlink(id)
+      ds = dates.all?(&:nil?) ? preferred_date : dates
+      ds.map do |d|
+        %Q|=HYPERLINK("#{[URL_BASE, id].join('/')}", "#{d.strftime('%H:%M')}")| if d
+      end
+    end
+
+    attr_reader :group, :user, :email, :title, :abstract, :preferred_date, :dates
   end
 end
 
@@ -30,6 +36,6 @@ if __FILE__ == $0
   seminars = KOF::Seminar.open(ARGV[0])
 
   seminars.each do |b|
-    p %i[group user email preferred_date].map{|i| b.__send__(i)}
+    p %i[group user email dates].map{|i| b.__send__(i)}
   end
 end

daea306b1e13e27d33efa85627b978e2ff34f14f
HIGAKI Masaru <mas.higa@gmail.com>
2019-11-02 16:43:45 +0900

    Fix poster.rb

--- poster.rb
+++ poster.rb
@@ -21,22 +21,51 @@ recommends =
 booth_of = Booth.open(ARGV[0]).inject({}){|bs, b| bs[b.group] = b; bs}
 
 def head
-  puts "<"
+  puts <<-EO_HEAD
+\\documentclass[14pt,mingoth]{jsarticle}
+\\usepackage[dvipdfm]{graphicx,color}
+
+\\pagestyle{empty}
+
+\\setlength{\\voffset}{-0.5in}
+\\setlength{\\hoffset}{-0.5in}
+
+\\setlength{\\textheight}{176mm}
+\\setlength{\\textwidth}{135mm}
+
+\\begin{document}
+\\LARGE
+  EO_HEAD
 end
 
-def body group, isbn
-  puts [group, isbn].join("\t")
+def body group, recommend
+  name = group.name
+  size = name.size > 15 ? "\\huge" : "\\Huge"
+  isbn = recommend.isbn.isbn
+  author = recommend.author&.empty? ? "" : recommend.author ? "\n\n\\vspace{1ex}\\hfill\\textcolor{red}{\\textbf{\\Huge サイン会開催}}" : ""
+  $stderr.puts [name, recommend.author].join("\t")
+  puts <<-EOF
+\\textbf{#{size} #{name}} 推薦#{author}
+\\begin{center}
+\\vfill
+\\includegraphics[height=10cm]{#{isbn}.jpg}
+\\vfill
+\\vfill
+\\includegraphics[height=8mm]{logo.pdf}
+\\end{center}
+\\pagebreak
+  EOF
 end
 
 def foot
-  puts ">"
+  puts '\\end{document}'
 end
 
 head
 
 recommends.each do |r|
   g = group_by_id[r.gid]
-  body(g.name, r.isbn.isbn) if g && booth_of[g.name]
+  body(g, r) if g && booth_of[g.name]
 end
 
 foot
